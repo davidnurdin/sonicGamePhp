ffcee4db7aa0bc5216c8849523176a2b05d9b0f2
David Nurdin <david.nurdin@respawnsive.com>
2025-05-09 23:26:49 +0200

    all level parsed

--- src/Level/LevelManager.php
+++ src/Level/LevelManager.php
@@ -25,6 +25,9 @@ class LevelManager extends EventEmitter
     {
         if ($this->currentLevelIndex < count($this->levels) - 1) {
             $this->currentLevelIndex++;
+            if ($this->currentLevelIndex == 20)
+                $this->currentLevelIndex++;
+
             $this->emit('levelChanged', [$this->currentLevelIndex+1]);
         }
     }
@@ -33,16 +36,48 @@ class LevelManager extends EventEmitter
     {
         if ($this->currentLevelIndex > 0) {
             $this->currentLevelIndex--;
+
+            if ($this->currentLevelIndex == 20)
+                $this->currentLevelIndex--;
+
             $this->emit('levelChanged', [$this->currentLevelIndex+1]);
         }
     }
 
 
+    public function convertLevelToTileSet($level)
+    {
+        $levelNTile[1] = 1 ; $levelNTile[2] = 2 ; $levelNTile[3] = 3 ; // green hill zone
+        $levelNTile[4] = 4 ; $levelNTile[5] = 5 ; $levelNTile[6] = 6 ; // bridge
+        $levelNTile[7] = 7 ; $levelNTile[8] = 8 ; $levelNTile[9] = 9 ; // jungle
+        $levelNTile[10] = 10 ; $levelNTile[11] = 11 ; $levelNTile[12] = 12 ; // labyrinth // tileset13 ??
+        $levelNTile[13] = 14 ; $levelNTile[14] = 15 ; $levelNTile[15] = 16 ; // Scrap Brain Act 1
+        $levelNTile[16] = 18 ; $levelNTile[17] = 19 ; // Sky Base
+        $levelNTile[18] = 26 ; // Skybase Boss
+        $levelNTile[19] = 1 ; // $155A4–A5	18: End Sequence
+//        $levelNTile[20] = 0 ; // unused level !
+        $levelNTile[21] = 14 ; // 20: Scrap Brain Act 2 (Emerald Maze), from corridor
+        $levelNTile[22] = 14 ; //	21: Scrap Brain Act 2 (Ballhog Area)
+        $levelNTile[23] = 14 ; // 22: Scrap Brain Act 2, from transporter
+        $levelNTile[24] = 14 ; // 23: Scrap Brain Act 2 (Emerald Maze), from transporter
+        $levelNTile[25] = 14 ; // 24: Scrap Brain Act 2, from Emerald Maze
+        $levelNTile[26] = 26 ; //25: Scrap Brain Act 2, from Ballhog Area
+        $levelNTile[27] = 27 ; // semble ok
+
+        $levelNTile[28] = 30 ; // BONUS !
+        $levelNTile[29] = 30 ; //25: Scrap Brain Act 2, from Ballhog Area
+        $levelNTile[30] = 30 ; $levelNTile[31] = 30 ; $levelNTile[32] = 30 ;$levelNTile[33] = 30 ;$levelNTile[34] = 30 ; $levelNTile[35] = 30 ; // bonus
+
+        return $levelNTile[$level];
+    }
+
     public function loadLevels()
     {
         for ($i = 1 ; $i < 30 ; $i++) {
+            if ($i == 20)
+                continue ;
             $tileSet = new TileSet($this->sdl);
-            $tileSet->loadTileSet('tileset' . $i, 'tileset/levels/tileset' . $i . '.png');
+            $tileSet->loadTileSet('tileset' . $i, 'tileset/levels/tileset' . $this->convertLevelToTileSet($i) . '.png');
             $level = new Level($tileSet,$i,$this->sdl);
             $level->readLevelPositionTilesAndMeta();
 
